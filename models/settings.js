b8acce952144df9016731aca9bc9a614242dd2b8
bondarenko-vlad <vladbondarenko173@gmail.com>
2021-11-11 18:35:22 +0300

    minor refactoring

--- models/settings.js
+++ models/settings.js
@@ -3,7 +3,7 @@ const { Schema, model, Types } = require("mongoose");
 const schema = new Schema({
   broadcastSubscribe: { type: Boolean },
   nightTheme: { type: Boolean },
-  owner: { type: Types.ObjectId, ref: "User" },
+  owner: { type: Types.ObjectId, ref: "User", unique: true },
 });
 
 module.exports = model("Settings", schema);
